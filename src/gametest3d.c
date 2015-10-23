ad1a8c2baaa47e9eeabf89e0dd5a883921609610
ljg6 <ljg6@njit.edu>
2015-10-23 15:48:25 -0400

    Map Finished/Objects on map

--- src/gametest3d.c
+++ src/gametest3d.c
@@ -77,7 +77,7 @@ Entity *newFloor(Vec3D position,const char *name)
     {
         return NULL;
     }
-    ent->objModel = obj_load("models/floor.obj");
+    ent->objModel = obj_load("models/demofloor.obj");
     ent->texture = LoadSprite("models/floorcarpet.png",1024,1024);
     vec3d_cpy(ent->body.position,position);
 	
@@ -131,6 +131,174 @@ Entity *newBox(Vec3D position,const char *name)
     return ent;
 }
 
+Entity *newDemoWall(Vec3D position,const char *name)
+{
+    Entity * ent;
+    ent = entity_new();
+    if (!ent)
+    {
+        return NULL;
+    }
+    ent->objModel = obj_load("models/techwall1.obj");
+    ent->texture = LoadSprite("models/walltexture.png",1024,1024);
+    vec3d_cpy(ent->body.position,position);
+	
+    cube_set(ent->body.bounds,-ent->objModel->size.x/2,-ent->objModel->size.y/2,-ent->objModel->size.z/2,ent->objModel->size.x,
+		ent->objModel->size.y,
+	ent->objModel->size.z);
+		
+    sprintf(ent->name,"%s",name);
+    mgl_callback_set(&ent->body.touch,touch_callback,ent);
+    return ent;
+}
+
+Entity *newDemoWallCenter(Vec3D position,const char *name)
+{
+    Entity * ent;
+    ent = entity_new();
+    if (!ent)
+    {
+        return NULL;
+    }
+    ent->objModel = obj_load("models/techwall4.obj");
+     ent->texture = LoadSprite("models/walltexture.png",1024,1024);
+    vec3d_cpy(ent->body.position,position);
+	
+    cube_set(ent->body.bounds,-ent->objModel->size.x/2,-ent->objModel->size.y/2,-ent->objModel->size.z/2,ent->objModel->size.x,
+		ent->objModel->size.y,
+	ent->objModel->size.z);
+		
+    sprintf(ent->name,"%s",name);
+    mgl_callback_set(&ent->body.touch,touch_callback,ent);
+    return ent;
+}
+
+Entity *newDemoDoor(Vec3D position,const char *name)
+{
+    Entity * ent;
+    ent = entity_new();
+    if (!ent)
+    {
+        return NULL;
+    }
+    ent->objModel = obj_load("models/techdoor.obj");
+    ent->texture = LoadSprite("models/seamless-wood-texture-free-35.png",1024,1024);
+    vec3d_cpy(ent->body.position,position);
+	
+    cube_set(ent->body.bounds,-ent->objModel->size.x/2,-ent->objModel->size.y/2,-ent->objModel->size.z/2,ent->objModel->size.x,
+		ent->objModel->size.y,
+	ent->objModel->size.z);
+		
+    sprintf(ent->name,"%s",name);
+    mgl_callback_set(&ent->body.touch,touch_callback,ent);
+    return ent;
+}
+
+Entity *newDemoTable(Vec3D position,const char *name)
+{
+    Entity * ent;
+    ent = entity_new();
+    if (!ent)
+    {
+        return NULL;
+    }
+    ent->objModel = obj_load("models/techtable.obj");
+     ent->texture = LoadSprite("models/metal_bare_0002_02_preview.png",1024,1024);
+    vec3d_cpy(ent->body.position,position);
+	
+    cube_set(ent->body.bounds,-ent->objModel->size.x/2,-ent->objModel->size.y/2,-ent->objModel->size.z/2,ent->objModel->size.x,
+		ent->objModel->size.y,
+	ent->objModel->size.z);
+		
+    sprintf(ent->name,"%s",name);
+    mgl_callback_set(&ent->body.touch,touch_callback,ent);
+    return ent;
+}
+
+Entity *newDemoGun(Vec3D position,const char *name)
+{
+    Entity * ent;
+    ent = entity_new();
+    if (!ent)
+    {
+        return NULL;
+    }
+    ent->objModel = obj_load("models/techwall4.obj");
+     ent->texture = LoadSprite("models/walltexture.png",1024,1024);
+    vec3d_cpy(ent->body.position,position);
+	
+    cube_set(ent->body.bounds,-ent->objModel->size.x/2,-ent->objModel->size.y/2,-ent->objModel->size.z/2,ent->objModel->size.x,
+		ent->objModel->size.y,
+	ent->objModel->size.z);
+		
+    sprintf(ent->name,"%s",name);
+    mgl_callback_set(&ent->body.touch,touch_callback,ent);
+    return ent;
+}
+
+Entity *newDemoKnife(Vec3D position,const char *name)
+{
+    Entity * ent;
+    ent = entity_new();
+    if (!ent)
+    {
+        return NULL;
+    }
+    ent->objModel = obj_load("models/techwall4.obj");
+     ent->texture = LoadSprite("models/walltexture.png",1024,1024);
+    vec3d_cpy(ent->body.position,position);
+	
+    cube_set(ent->body.bounds,-ent->objModel->size.x/2,-ent->objModel->size.y/2,-ent->objModel->size.z/2,ent->objModel->size.x,
+		ent->objModel->size.y,
+	ent->objModel->size.z);
+		
+    sprintf(ent->name,"%s",name);
+    mgl_callback_set(&ent->body.touch,touch_callback,ent);
+    return ent;
+}
+
+Entity *newDemoBrass(Vec3D position,const char *name)
+{
+    Entity * ent;
+    ent = entity_new();
+    if (!ent)
+    {
+        return NULL;
+    }
+    ent->objModel = obj_load("models/techwall4.obj");
+     ent->texture = LoadSprite("models/walltexture.png",1024,1024);
+    vec3d_cpy(ent->body.position,position);
+	
+    cube_set(ent->body.bounds,-ent->objModel->size.x/2,-ent->objModel->size.y/2,-ent->objModel->size.z/2,ent->objModel->size.x,
+		ent->objModel->size.y,
+	ent->objModel->size.z);
+		
+    sprintf(ent->name,"%s",name);
+    mgl_callback_set(&ent->body.touch,touch_callback,ent);
+    return ent;
+}
+
+Entity *newPlayer(Vec3D position,const char *name)
+{
+    Entity * ent;
+    ent = entity_new();
+    if (!ent)
+    {
+        return NULL;
+    }
+    ent->objModel = NULL;
+     ent->texture = NULL;
+    vec3d_cpy(ent->body.position,position);
+	
+    cube_set(ent->body.bounds,-ent->objModel->size.x/2,-ent->objModel->size.y/2,-ent->objModel->size.z/2,ent->objModel->size.x,
+		ent->objModel->size.y,
+	ent->objModel->size.z);
+		
+    sprintf(ent->name,"%s",name);
+    mgl_callback_set(&ent->body.touch,touch_callback,ent);
+    return ent;
+}
+
 int main(int argc, char *argv[])
 {
     int i;
@@ -139,6 +307,10 @@ int main(int argc, char *argv[])
     Entity *cube1,*cube2;
 	Entity *floor;
 	Entity *wall1, *wall2, *wall3, *wall4;
+	Entity *techwall1, *techwall2, *techwall3, *techwall4, *techwall5, *techwall6, *techwall7, *techwall8;
+	Entity *techdoor1;
+	Entity *wallCenter1, *wallCenter2;
+	Entity *demoTable1;
 	Entity *box1;
     char bGameLoopRunning = 1;
     Vec3D cameraPosition = {0,-10,0.3};
@@ -164,32 +336,56 @@ int main(int argc, char *argv[])
     //bgtext = LoadSprite("models/mountain_text.png",1024,1024);
     
     floor = newFloor(vec3d(0,0,0),"TheFloor");  //vec3d(-2.26,-2.26,0)
-	wall1 = newWall(vec3d(1.5,-1.5,1.5),"Wall 1");
+	techwall1 = newDemoWall(vec3d(0,0,0),"Wall 1");
+	techwall2 = newDemoWall(vec3d(8,0,0),"Wall 2");
+	techwall3 = newDemoWall(vec3d(0,4,0),"Wall 3");
+	techwall4 = newDemoWall(vec3d(4,4,0),"Wall 4");
+	techwall5 = newDemoWall(vec3d(4,0,0),"Wall 5");
+	techwall6 = newDemoWall(vec3d(0,0,0),"Wall 6");
+	techdoor1 = newDemoDoor(vec3d(0,0,0),"Door 1");
+	wallCenter1 = newDemoWallCenter(vec3d(0,0,0),"Wall Center1");
+	wallCenter2 = newDemoWallCenter(vec3d(0,2.45,0),"Wall Center2");
+	demoTable1 = newDemoTable(vec3d(0,0,0),"Demo Table");
+
+	techwall3->rotation =vec3d(0,0,90);
+	techwall4->rotation =vec3d(0,0,90);
+	techwall5->rotation =vec3d(0,0,90);
+	techwall6->rotation =vec3d(0,0,90);
+
+	/*wall1 = newWall(vec3d(1.5,-1.5,1.5),"Wall 1");
 	wall2 = newWall(vec3d(0,0,.5),"Wall 2");
 	wall2->rotation =vec3d(0,0,90);
 	wall3 = newWall(vec3d(-1.5,-1.5,1.5),"Wall 3");
 	//wall3->rotation =vec3d(0,0,90);
-	//wall4 = newWall(vec3d(0,0,0),"Wall 4",vec3d(-2.26,-2.26,0),vec3d(4.52,4.52,1));
+	//wall4 = newWall(vec3d(0,0,0),"Wall 4",vec3d(-2.26,-2.26,0),vec3d(4.52,4.52,1));*/
 	cube1 = newCube(vec3d(10,-1.2,.5),"Cubert");  //(vec3d(0,-1.2,0)
-    cube2 = newCube(vec3d(0,-1.2,5.27),"Hobbes");
+    //cube2 = newCube(vec3d(0,-1.2,5.27),"Hobbes");
 	//cube1->rotation = vec3d(0,0,0);
 	//cube2->rotation = vec3d(0,0,0);
 	//box1 = newBox(vec3d(0,0,3),"Box 1");
 	
     
-    cube2->body.velocity.x = 0.1;
+    //cube2->body.velocity.x = 0.1;
 	cube1->body.velocity.x = -0.1;
-	wall1->body.velocity.z = -0.1;
-	wall2->body.velocity.z = -0.1;
-	wall3->body.velocity.z = -0.1;
+	//wall1->body.velocity.z = -0.1;
+	//wall2->body.velocity.z = -0.1;
+	//wall3->body.velocity.z = -0.1;
     
     space = space_new();
     space_set_steps(space,100);
     
     space_add_body(space,&floor->body);
 	space_add_body(space,&cube1->body);
-    space_add_body(space,&cube2->body);
-	space_add_body(space,&wall1->body);
+    //space_add_body(space,&cube2->body);
+	space_add_body(space,&techwall1->body);
+	space_add_body(space,&techwall2->body);
+	space_add_body(space,&techwall3->body);
+	space_add_body(space,&techwall4->body);
+	space_add_body(space,&techwall5->body);
+	space_add_body(space,&techwall6->body);
+	space_add_body(space,&demoTable1->body);
+	space_add_body(space,&wallCenter1->body);
+	//space_add_body(space,&wall1->body);
     while (bGameLoopRunning)
     {
         for (i = 0; i < 100;i++)
@@ -277,6 +473,40 @@ int main(int argc, char *argv[])
                     cameraRotation.x -= 1;
                 }
             }
+			else if (e.type == SDL_MOUSEMOTION)
+			{
+				if(e.motion.y < 192)
+				{
+					cameraRotation.x += 1;
+				}
+				else if(e.motion.y > 496)
+				{
+					cameraRotation.x -= 1;
+				}
+				if(e.motion.x <256)
+				{
+					cameraRotation.z += 1;
+				}
+				else if(e.motion.x > 768)
+				{
+					cameraRotation.z -= 1;
+				}
+
+			}
+			else if(e.type == SDL_MOUSEBUTTONDOWN)
+			{
+				if(e.button.button == SDL_BUTTON_LEFT)
+				{
+					vec3d_add(
+                        cameraPosition,
+                        cameraPosition,
+                        vec3d(
+                            -sin(cameraRotation.z * DEGTORAD),
+                            cos(cameraRotation.z * DEGTORAD),
+                            0
+                        ));
+				}
+			}
         }
 
         graphics3d_frame_begin();
